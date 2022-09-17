7b6ab97ab3e829a2b908238b2b54eff543f83c5d
김소연 <jejen205@naver.com>
2022-09-18 00:21:58 +0900

    로그인, 회원가입 페이지 router 연결

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,12 +1,18 @@
-import {Route, Routes} from "react-router-dom"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login1 from "../pages/Login1";
+import Login2 from "../pages/Login2";
+import SignUp from "../pages/SignUp";
 
 
 function Router(){
     return(
+        <BrowserRouter>
         <Routes>
-
+            <Route path="/login" element= {<Login1/>}/>
+            <Route path="/login2" element= {<Login2/>}/>
+            <Route path="/signup" element={<SignUp/>}/>
         </Routes>
+        </BrowserRouter>
     )
 }
 
