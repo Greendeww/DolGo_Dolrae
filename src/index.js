cbb22bc971670ab3efa485b34787ab237424c3c3
Greendeww <110080219+Greendeww@users.noreply.github.com>
2022-09-19 20:46:23 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 import { Provider } from "react-redux";
@@ -10,9 +9,7 @@ import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
         <App />
-    </BrowserRouter>
   </Provider>
 );
 
