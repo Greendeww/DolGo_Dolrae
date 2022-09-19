dc401bdc4ddd927ddc44db5e2f320f912cff2db9
Ermael35 <ermael35@gmail.com>
2022-09-19 20:54:15 +0900

    commit

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,13 +1,14 @@
-import {Route, Routes} from "react-router-dom"
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-
-
-function Router(){
-    return(
-        <Routes>
-
-        </Routes>
-    )
-}
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default Router;
