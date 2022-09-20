caaf00bbea55144b23c61d990516370ebd146b32
김소연 <jejen205@naver.com>
2022-09-21 00:09:42 +0900

    테마페이지 컴포넌트 수집

--- src/pages/Theme.jsx
+++ src/pages/Theme.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import Category from "../componenets/theme/Category";
 import Location from "../componenets/theme/Location";
-import List from "../componenets/theme/List";
 
 const Theme = () => {
   return (
     <StTheme>
       <Category />
       <Location />
-      <List />
     </StTheme>
+    
   );
 };
 
