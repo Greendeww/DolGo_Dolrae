3cfb49ac67515d7bd917087561cc48c0971e386c
김소연 <jejen205@naver.com>
2022-09-21 00:16:44 +0900

    로그인, 회원가입 리듀서

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -1,5 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { instance } from "../../shared/Api";
+import { setCookie } from "../../shared/Cookie";
 
 const initialState = {
   users: [],
@@ -7,61 +9,101 @@ const initialState = {
   error: null,
 };
 
-export const __signUp = createAsyncThunk(
-  "user/__signUp",
-  async (user, thunkAPI) => {
-    try {
-      const response = await axios.post("/api/member/signup", user);
-      return response.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error);
-    }
-  }
-);
+// export const __signUp = createAsyncThunk(
+//   "user/__signUp",
+//   async (user, thunkAPI) => {
+//     try {
+//       const response = await axios.post("http://localhost:3001/users", user);
+//       return response.data;
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error);
+//     }
+//   }
+// );
 
-export const __emailCheck = createAsyncThunk(
-  "user/__idCheck",
+// export const __emailCheck = createAsyncThunk(
+//   "user/__idCheck",
+//   async (user, thunkAPI) => {
+//     try {
+//       const response = await instance.post("", user);
+//       return response.data;
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error);
+//     }
+//   }
+// );
+
+export const __login = createAsyncThunk(
+  "user/__login",
   async (user, thunkAPI) => {
     try {
-      const response = await axios.post("", user);
+      const response = await instance.post("/api/member/login", user);
+      if (response.data.success) {
+        setCookie("isLogin", response.headers.Authorization);
+        setCookie("ACCESS_TOKEN", response.headers.Authorization);
+        setCookie("REFRESH_TOKEN", response.headers.Refreshtoken);
+        localStorage.setItem("email", response.data.data.email);
+      }
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
   }
-);
+)
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(__signUp.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(__signUp.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.users = action.payload;
-        window.alert("회원가입 되었습니다.");
-        console.log(action.payload);
-      })
-      .addCase(__signUp.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      });
+  reducers: {
+    signUp: (state,action) => {
+      instance.post("/api/member/signup", action.payload);
+      state.users.push(action.payload);
+    },
 
-    builder
-      .addCase(__emailCheck.pending, (state) => {
+  },
+  extraReducers: (builder) => {
+    // builder
+    //   .addCase(__signUp.pending, (state) => {
+    //     state.isLoading = true;
+    //   })
+    //   .addCase(__signUp.fulfilled, (state, action) => {
+    //     state.isLoading = false;
+    //     state.users = action.payload;
+    //     window.alert("회원가입 되었습니다.");
+    //     console.log(action.payload);
+    //   })
+    //   .addCase(__signUp.rejected, (state, action) => {
+    //     state.isLoading = false;
+    //     state.error = action.payload;
+    //   });
+
+    // builder
+    //   .addCase(__emailCheck.pending, (state) => {
+    //     state.isLoading = true;
+    //   })
+    //   .addCase(__emailCheck.fulfilled, (state, action) => {
+    //     state.isLoading = false;
+    //     state.users = action.payload;
+    //     window.alert("사용 가능한 이메일입니다.");
+    //     console.log(action.payload);
+    //   })
+    //   .addCase(__emailCheck.rejected, (state, action) => {
+    //     state.isLoading = false;
+    //     window.alert("이미 존재하는 이메일입니다..");
+    //     state.error = action.payload;
+    //   });
+
+      builder
+      .addCase(__login.pending, (state) => {
         state.isLoading = true;
       })
-      .addCase(__emailCheck.fulfilled, (state, action) => {
+      .addCase(__login.fulfilled, (state, action) => {
         state.isLoading = false;
         state.users = action.payload;
-        window.alert("중복된 이메일입니다.");
+        window.alert("로그인 되었습니다.");
         console.log(action.payload);
       })
-      .addCase(__emailCheck.rejected, (state, action) => {
+      .addCase(__login.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.payload;
       });
