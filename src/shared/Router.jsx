7752898fd33b8ed6588ca723aaa4e1013949d2d8
Yunjae <nuguri206@gmail.com>
2022-09-23 16:57:09 +0900

    <bug>: 상세페이지 서버 연결 안됨

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -4,6 +4,7 @@ import Login2 from "../pages/Login2";
 import SignUp from "../pages/SignUp";
 import Detail from "../pages/Detail";
 import DetailRevise from "../componenets/details/DetailRevise";
+import DetailForm from "../componenets/details/DetailForm";
 
 
 
@@ -16,6 +17,7 @@ function Router(){
             <Route path="/signup" element={<SignUp/>}/>
             <Route path="/detail/:id" element={<Detail />} />
             <Route path="/detail/update/:placeId/:id" element={<DetailRevise/>}/>
+            <Route path="/detail/form/:id" element={<DetailForm/>}/>
         </Routes>
         </BrowserRouter>
     )
