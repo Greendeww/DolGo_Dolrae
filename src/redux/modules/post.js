4d27c0d02c83709e7bdab0795423bd869b454980
김소연 <jejen205@naver.com>
2022-09-30 15:01:21 +0900

    중간 전체커밋

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -1,19 +1,20 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
+import { instance } from "../../shared/Api";
 
 const initialState = {
-    post: [],
-    detail:{},
-    isLoading: false,
-    error: null,
-}
+  post: [],
+  detail: {},
+  isLoading: false,
+  error: null,
+};
 
 export const _getDetail = createAsyncThunk(
   "post/getDetail",
   async (payload, thunkAPI) => {
     try {
       const data = await axios.get(
-        `http://13.125.38.46:8080/api/place/${payload}`,
+        `http://13.125.38.46:8080/api/place/${payload}`
         // {
         //   headers: {
         //     Authorization: localStorage.getItem("Authorization"),
@@ -28,6 +29,7 @@ export const _getDetail = createAsyncThunk(
     }
   }
 );
+
 export const onLikeDetail = createAsyncThunk(
   "like/onLikePost",
   async (payload, thunkApI) => {
@@ -43,7 +45,7 @@ export const onLikeDetail = createAsyncThunk(
           },
         } //post는 두번째 인자가 데이터가 들어가야해서 {}를 넣어줌 데이터가 없으면 headers를 데이터로 인식
       );
-      window.location.reload()
+      window.location.reload();
       return payload;
     } catch (error) {
       return thunkApI.rejectWithValue(error);
@@ -52,25 +54,25 @@ export const onLikeDetail = createAsyncThunk(
 );
 
 export const postSlice = createSlice({
-    name:"post",
-    initialState,
-    reducers:{},
-extraReducers:(builder) => {
-        builder
-            .addCase(_getDetail.pending, (state) => {
-                state.isLoading = true;
-            })
-            .addCase(_getDetail.fulfilled, (state,action) => {
-                state.isLoading = false;
-                state.detail = action.payload;
-                console.log(state.detail)
-            })
-            .addCase(_getDetail.rejected, (state,action) => {
-                state.isLoading = false;
-                state.error = action.payload;
-            })
-    }
-})
+  name: "post",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(_getDetail.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(_getDetail.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.detail = action.payload;
+        console.log(state.detail);
+      })
+      .addCase(_getDetail.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
+});
 
 export const {} = postSlice.actions;
-export default postSlice
+export default postSlice;
