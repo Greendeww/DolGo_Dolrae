49f09c5eb9eb367d33abf7cfc1997d71f216afc3
김소연 <jejen205@naver.com>
2022-10-05 21:45:54 +0900

    font 설정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import Router from "./shared/Router";
 import { Provider } from "react-redux";
 import store from "./redux//store";
+import styled from "styled-components";
 
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Provider store={store}>
+      <Provider store={store} style={{ fontFamily: "tway"}}>
         <Router />
       </Provider>
-    </>
   );
 }
 
