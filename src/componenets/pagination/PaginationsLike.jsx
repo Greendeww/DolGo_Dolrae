576a2d16881750bcba16fc8af909a0b818b570d8
Yunjae <nuguri206@gmail.com>
2022-10-06 21:34:59 +0900

    feat : 마이페이지 페이지네이션 작업

--- src/componenets/pagination/PaginationsLike.jsx
+++ src/componenets/pagination/PaginationsLike.jsx
@@ -2,18 +2,15 @@ import React,{useState} from 'react'
 import Pagination from "react-js-pagination";
 import '../../css/paging.css';
 
-const Paginations = ({page, count, setPage,postPerPage}) => {
+const PaginationsLike = ({page, count, setPage,postPerPage}) => {
   // const [page, setPage] = useState(1);
-  const handlePageChange = (page) => {
-    setPage(page);
-  };
 
 
   return (
     <>
         <Pagination
             activePage={page} //현재페이지
-            itemsCountPerPage={3} //한 페이지당 보여줄 리스트 아이템 개수
+            itemsCountPerPage={2} //한 페이지당 보여줄 리스트 아이템 개수
             totalItemsCount={count ? count : 0} // 총 아이템 개수
             pageRangeDisplayed={5} // paginator내에서 보여줄 페이지의 범위
             prevPageText={"‹"} 
@@ -25,4 +22,4 @@ const Paginations = ({page, count, setPage,postPerPage}) => {
 }
 
 
-export default Paginations
+export default PaginationsLike
