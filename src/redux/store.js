6eefd24050df71e7a987eefb41d6babbf25aeb49
김소연 <jejen205@naver.com>
2022-10-26 13:20:03 +0900

    conflict 해결

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import commentSlice from "./modules/comment";
 import postSlice from "./modules/post";
+import coseSlice from "./modules/coses";
 
 const store = configureStore({
   reducer: {
     post: postSlice.reducer,
     comment: commentSlice.reducer,
-    theme: themeSlice.reducer,
     cose: coseSlice.reducer
   },
 });
