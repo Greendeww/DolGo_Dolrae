1bdcd2dbf7758276b1ae3483e6ea0a3961995b60
Yunjae <nuguri206@gmail.com>
2022-10-26 13:45:15 +0900

    fix: 마이페이지 지도 모달 버튼 짤리는 오류 해결

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import commentSlice from "./modules/comment";
+import coseSlice from "./modules/coses";
 import postSlice from "./modules/post";
 
 const store = configureStore({
   reducer: {
     post: postSlice.reducer,
     comment: commentSlice.reducer,
-    theme: themeSlice.reducer,
     cose: coseSlice.reducer
   },
 });
